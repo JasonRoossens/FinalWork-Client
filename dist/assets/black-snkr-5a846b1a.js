793569e1fe8115acc4e3c9f0115e779560a2408f
JasonRoossens <jason.roossens@student.ehb.be>
2023-08-22 16:31:37 +0200

    update

--- dist/assets/black-snkr-5a846b1a.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/black-snkr-e097a5c5.jpg";export{s as _};
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/FinalWork-Client/',
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src') // Use path.resolve to get the absolute path
     }
   }
-})
+});
